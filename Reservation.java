04aebd62366b998926e8a0b3edb7be5aaac3df04
David Yin <29613693+E137299@users.noreply.github.com>
2023-11-10 08:51:16 -0600

    Create Reservation.java

---
+++ Reservation.java
@@ -0,0 +1,25 @@
+public class Reservation{
+    private String guestName;
+    private int roomNumber;
+
+    public Reservation(String guestName, int roomNumber){
+        this.guestName = guestName;
+        this.roomNumber = roomNumber;
+    }
+
+    public void setGuestName(String name){
+        this.guestName = name;
+    }
+
+    public void setRoomNumber(int number){
+        this.roomNumber = number;
+    }
+
+    public String getGuestName(){
+        return this.guestName;
+    }
+
+    public int getRoomNumber(){
+        return this.roomNumber;
+    }
+}
